6691874899c1c80669af03d7840b26ec8922b1d8
James Pickering <jpuidev@outlook.com>
2024-09-29 20:44:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const accounts = [
-  { id: 0, name: 'chun', last: 'li', age: 25, country: 'china' },
-  { id: 1, name: 'sam', last: 'baker', age: 20, country: 'united kingdom' },
-  { id: 2, name: 'samuel', last: 'jackson', age: 60, country: 'united states' },
+  { id: 0, username: 'chun', last: 'li', age: 25, country: 'china' },
+  { id: 1, username: 'sam', last: 'baker', age: 20, country: 'united kingdom' },
+  { id: 2, username: 'samuel', last: 'jackson', age: 60, country: 'united states' },
 ];
-const matched = accounts.find((element) => element.name.includes('sam'));
+const matched = accounts.find((element) => element.username.includes('sam'));
 console.log('first matched', matched);
 // first matched { id: 1, name: 'sam', last: 'baker', age: 20, country: 'united kingdom' },
 
-const filtered = accounts.filter((element) => element.name.includes('sam'));
+const filtered = accounts.filter((element) => element.username.includes('sam'));
 console.log('filtered', filtered);
 
 const ages = filtered.map((element) => element.age);
